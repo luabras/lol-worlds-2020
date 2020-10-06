68c172b63acfa9d55cb8cadf61733dad7595e062
NayeliFeitosa <you@example.com>
2020-10-06 12:41:58 -0300

    Agenda_fase_eliminatoria

--- models/example.py
+++ models/example.py
@@ -13,7 +13,7 @@ Escolha sua opção
 
     if escolha1 == '1':
         while True:
-            escolha1 = input("""~ Tabela de Jogos - Fase Eliminatória ~
+            escolha1 = input("""~ Tabela de Jogos - Fase de Grupos ~
 Escolha sua opção:
 1 - Sábado  - 3 de outubro 
 2 - Domindo - 4 de outubro 
@@ -32,6 +32,7 @@ Escolha sua opção:
                     8h - DAMWON Gaming x JD Gaming
                     9h - Gen.G x LGD Gaming
                     10h - TSM x Fnatic \n""")
+                back = input("Digite enter para voltar pro menu anterior")
 
 
             elif escolha1 == '2':
@@ -42,6 +43,7 @@ Escolha sua opção:
                     8h - PSG Talon x JD Gaming
                     9h - Gen.G x TSM
                     10h - LGD Gaming x Fnatic \n""")
+                back = input("Digite enter para voltar pro menu anterior")
 
 
             elif escolha1 == '3':
@@ -52,6 +54,7 @@ Escolha sua opção:
                     8h - JD Gaming x Rogue
                     9h - FlyQuest x Unicorns of Love
                     10h - Top Esports x DragonX \n""")
+                back = input("Digite enter para voltar pro menu anterior")
 
 
             elif escolha1 == '4':
@@ -62,6 +65,7 @@ Escolha sua opção:
                     8h - Top Esports x Unicorns of Love
                     9h - Fnatic x Gen.G
                     10h - TSM x LGD Gaming \n""")
+                back = input("Digite enter para voltar pro menu anterior")
 
             elif escolha1 == '5':
                 print("""~ Jogos do dia 08 - Quinta ~
@@ -71,6 +75,7 @@ Escolha sua opção:
                     8h - Suning x Team Liquid
                     9h - Team Liquid x Machi Esports
                     10h - Suning x G2 \n""")
+                back = input("Digite enter para voltar pro menu anterior")
 
             elif escolha1 == '6':
                 print("""~ Jogos do dia 09 - Sexta ~
@@ -80,6 +85,7 @@ Escolha sua opção:
                     8h - JD Gaming x PSG Talon
                     9h - PSG Talon x Rogue
                     10h - JD Gaming x DAMWON Gaming \n""")
+                back = input("Digite enter para voltar pro menu anterior")
 
             elif escolha1 == '7':
                 print("""~ Jogos do dia 10 - Sábado ~
@@ -89,12 +95,44 @@ Escolha sua opção:
                     8h - Fnatic x LGD Gaming
                     9h - LGD Gaming x TSM
                     10h - Gen.G x Fnatic \n""")
+                back = input("Digite enter para voltar pro menu anterior")
 
             elif escolha1 == '0':
                 break
 
+    if escolha1 == '2':
+        while True:
+            escolha1 = input("""~ Tabela de Jogos - Fase Eliminatória ~
+Escolha sua opção:
+1 - Quinta  - 15 de outubro 
+2 - Sexta   - 16 de outubro 
+3 - Sábado  - 17 de outubro
+4 - Domingo - 18 de outubro
+0 - Voltar para o menu anterior
+►  """)
+            if escolha1 == '1':
+                seed1 = input ("Insira o vencedor do seed 1: ")
+                seed2 = input("Insira o vencedor do seed 2: ")
+                print ("7h - Quartas de Final 1 - {} x {}".format(seed1, seed2))
+                back = input("Digite enter para voltar pro menu anterior")
 
+            if escolha1 == '2':
+                seed3 = input ("Insira o vencedor do seed 3: ")
+                seed4 = input ("Insira o vencedor do seed 4: ")
+                print ("7h - Quartas de Final 2 - {} x {}" .format(seed3, seed4))
+                back = input("Digite enter para voltar pro menu anterior")
 
+            if escolha1 == '3':
+                seed5 = input ("Insira o vencedor do seed 5: ")
+                seed6 = input ("Insira o vencedor do seed 6: ")
+                print ("7h - Quartas de Final 3 - {} x {}" .format(seed5, seed6))
+                back = input("Digite enter para voltar pro menu anterior")
+
+            if escolha1 == '4':
+                seed7 = input ("Insira o vencedor do seed 7: ")
+                seed8 = input ("Insira o vencedor do seed 8: ")
+                print ("7h - Quartas de Final 4 - {} x {}" .format(seed7, seed8))
+                back = input("Digite enter para voltar pro menu anterior")
 
 
 
